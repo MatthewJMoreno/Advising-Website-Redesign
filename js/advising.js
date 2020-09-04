de0b4998ca53257c48c13f1a97499e7579d5bf0f
Matt Moreno <matthewjmoreno22@yahoo.com>
2020-09-04 14:11:17 -0600

    init

---
+++ js/advising.js
@@ -0,0 +1,5 @@
+let home = doucment.querySelector('#home');
+let whatAdvisersDoTab = document.querySelector('#what-advisors-do-tab');
+let whoIsMyAdviserTab = docment.querySelector('#who-is-my-advisor-tab');
+let makeAnAppointmentTab = docment.querySelector('#make-an-appointment-tab');
+let forAdviors = docment.querySelector('#for-advisors-tab');  
