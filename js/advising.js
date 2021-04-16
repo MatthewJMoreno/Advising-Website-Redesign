486a27684919fb0e345fefc40ee4ca0526ef5aff
MatthewJMoreno <mmoreno@rams.colostate.edu>
2021-04-16 13:48:38 -0600

    finished homepage

--- js/advising.js
+++
@@ -1,9 +0,0 @@
-$('.active').each(function(index) {
-    this.classList.remove('active');
-});
-
-if (document.URL.includes('home')){
-    document.querySelector('#home').classList.add('active');
-} else if (document.URL.includes('what-advisors-do')) {
-    document.querySelector('#what-advisors-do-tab').classList.add('active');
-} 
