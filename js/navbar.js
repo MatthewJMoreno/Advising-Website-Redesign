754a522656fa5bb5f0b4cccad014fd395c0cfa2e
MatthewJMoreno <mmoreno@rams.colostate.edu>
2021-05-10 14:47:58 -0600

    new-changes

--- js/navbar.js
+++ js/navbar.js
@@ -2,8 +2,8 @@ let url = window.location.href;
 
 if (url.includes('home')){
   document.getElementById('home-tab').classList.add('current-link');
-} else if (url.includes('when')){
-  document.getElementById('when-to-seek-advising-tab').classList.add('current-link');
+} else if (url.includes('roles')){
+  document.getElementById('roles-and-responsibilities-tab').classList.add('current-link');
 } else if (url.includes('find')){
   document.getElementById('find-advising-tab').classList.add('current-link');
 } else if (url.includes('additional')){
